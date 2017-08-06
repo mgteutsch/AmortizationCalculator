c2f3ea525453ee235c8e8293efc43587ec922327
Mike Teutsch <mgt0117@gmail.com>
2017-08-06 14:57:33 -0500

    Affordability set up, calculation just needs fixed

--- javascripts/Controllers/AffordabilityCtrl.js
+++ javascripts/Controllers/AffordabilityCtrl.js
@@ -2,8 +2,29 @@
 
 app.controller("AffordabilityCtrl", function($q, $scope, $rootScope, $location){
 
-console.log("Affordability is linked");
+	//To handle certain dynamic items in Materialize, they must be initialized:
+	$(document).ready(function () {
+		$('select').material_select();
+		$('.collapsible').collapsible();
+	});    
+	//*************************************************************************
 
+	var monthlyPayment = 0;
+	var interestRate = 0;
+	var monthlyTerm = 0;
+
+	$scope.showAffordabilityResult = false;
+
+	$scope.calculateTotalAffordability = function() {
+		monthlyPayment = $scope.MonthlyPayment;
+		interestRate = $scope.InterestRate;
+		monthlyTerm = $scope.TermMonths;
+
+		$scope.AffordabilityResult = (monthlyPayment / (interestRate / (1 - Math.pow((1 / (1 + interestRate)), monthlyTerm))));
+
+		$scope.showAffordabilityResult = true;
+
+	}
 
 
 });
